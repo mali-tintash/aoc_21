079c308cd0354ce26f2d7ab9169112253fd49129
Muhammad Ali <53075248+mali-tintash@users.noreply.github.com>
2022-07-20 18:33:03 +0500

    Challenge 2 for AoC21 completed (#2)

---
+++ aoc_21/src/challenges/chal2.rs
@@ -0,0 +1,38 @@
+
+pub fn larger_window_count(path: &str) -> usize {
+
+    
+
+    let bigger_window = include_str!("../../chal1_input.txt")
+                    .lines()
+                    .map(|x| x.parse().unwrap())
+                    .collect::<Vec<u16>>()
+                    .windows(3)
+                    .map(|x| x[0] + x[1] + x[2])
+                    .collect::<Vec<u16>>()
+                    .windows(2)
+                    .filter(|x| x[0] < x[1])
+                    .count();
+
+     //let p = bigger_window.windows(2);
+    //         .windows(3);
+
+    // let windows = p.clone();
+    // for w in p.into_iter() {
+    //     println!("{:?} ->",w);
+    // }
+
+    //let sum_of_windows : Vec<u16> = windows.map(|x| x[0] + x[1] + x[2]).collect();
+
+   
+    
+    return bigger_window;
+}
+
+// pub fn get_int_from_result(input: Option<Result<String,std::io::Error>>) -> i32{
+
+//     if let Some(el) = input {
+//         el.unwrap().parse::<i32>().unwrap();
+//     }
+//     return 0;
+// }
--- aoc_21/src/challenges/mod.rs
+++ aoc_21/src/challenges/mod.rs
@@ -1,1 +1,3 @@
+
 pub mod chal1;
+pub mod chal2;
