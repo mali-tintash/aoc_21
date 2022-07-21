9ab237e2ce7c1fa689d125492f6cae69e1faad6f
Muhammad Ali <53075248+mali-tintash@users.noreply.github.com>
2022-07-21 10:55:33 +0500

    removed some warnings (#5)

--- aoc_21/src/challenges/chal1.rs
+++ aoc_21/src/challenges/chal1.rs
@@ -2,17 +2,11 @@
 use aoc_21::read_lines;
 
 
-enum FileError
-{
-    FileError1
-}
-
-
 pub fn larger_measurements_count(path: &str) -> i32 {
     //let f = fs::read_to_string(path).unwrap(); 
-    let mut p1 = -1;
+    let mut p1: i32;
     let mut p2 = -1;
-    let mut biggerCount = 0;
+    let mut bigger_count = 0;
 
 
     if let Ok(lines) = read_lines(path){
@@ -22,7 +16,7 @@ pub fn larger_measurements_count(path: &str) -> i32 {
                 p1 = entry.parse::<i32>().unwrap();
                 if p2 >= 0 {
                     if p1 > p2 {
-                        biggerCount += 1;
+                        bigger_count += 1;
                     }
                 }
                 p2 = p1;
@@ -32,5 +26,5 @@ pub fn larger_measurements_count(path: &str) -> i32 {
     }
     
 
-    return biggerCount;
+    return bigger_count;
 }
--- aoc_21/src/main.rs
+++ aoc_21/src/main.rs
@@ -7,7 +7,7 @@ use challenges::chal2;
 fn main() {
     println!("Welcome to AOC 21");
     let path = "./chal1_input.txt";
-    let bigCount = chal1::larger_measurements_count(path);
-    println!("challenge 1: {}\n", bigCount);
+    let big_count = chal1::larger_measurements_count(path);
+    println!("challenge 1: {}\n", big_count);
     println!("challenge 2: {}\n", chal2::larger_window_count(path));
 }
